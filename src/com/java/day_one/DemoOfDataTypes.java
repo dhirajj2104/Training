85c749d851822f187f354d24dbadaa63ab5c6046
Dhiraj Jadhav <dhirajj2104@gmail.com>
2026-03-13 19:52:41 +0530

    commit

---
+++ src/com/java/day_one/DemoOfDataTypes.java
@@ -0,0 +1,19 @@
+package com.java.day_one;
+
+public class DemoOfDataTypes {
+	public static void main(String[] args) {
+		
+		int val1 = 13/2;
+		System.out.println(val1);
+		
+		float val2 = 356f / 97f;
+		System.out.println(val2);
+		
+		double val3 = 356d / 97d;
+		System.out.println(val3);
+		
+		int r = 16;
+		double cirumference = 2*3.14*r;
+		System.out.println(cirumference);
+	}
+}
---
+++ src/com/java/day_one/DemoPrimitiveDataTypes.java
@@ -0,0 +1,29 @@
+package com.java.day_one;
+
+public class DemoPrimitiveDataTypes {
+	public static void main(String[] args) {
+		
+		byte byteMax = 127;
+		byte byteMin = -127;
+		System.out.println("maximum range of byte is "+ byteMax);
+		System.out.println("minimum range of byte is "+ byteMin);
+		
+		
+		short shortMax = 32767;
+		short shortMin = -32768;
+		System.out.println("maximum range of short is "+ shortMax);
+		System.out.println("minimum range of short is "+ shortMin);
+		
+		
+		int intMax = -2147483648;
+		int intMin = 2147483647;
+		System.out.println("maximum range of int is "+ intMax);
+		System.out.println("minimum range of int is "+ intMin);
+		
+		
+		long longMax = 9223372036854775807L;
+		long longMin = -9223372036854775808L;
+		System.out.println("maximum range of byte is "+ longMax);
+		System.out.println("minimum range of byte is "+ longMin);
+	}
+}
