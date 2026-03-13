83404bd0900192616f0e58681f2282829bc5e4af
Dhiraj Jadhav <dhirajj2104@gmail.com>
2026-03-13 19:57:23 +0530

    commit

---
+++ src/com/java/day_two/DemoOfIfElse.java
@@ -0,0 +1,15 @@
+package com.java.day_two;
+
+public class DemoOfIfElse {
+	public static void main(String[] args) {
+	
+		int age = 16;
+		
+		if(age >= 21) {
+			System.out.println("Eligible for masters");
+		}
+		else {
+			System.out.println("Not Eligible for masters");
+		}
+	}
+}
